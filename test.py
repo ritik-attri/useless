c94de0b89db5d02c7da111122e0b1d4df12df123
ritik-attri <attri.ritik1@gmail.com>
2020-09-30 19:29:53 +0530

    deleting

--- test.py
+++
@@ -1,9 +0,0 @@
-#example to get response using python
-
-import requests 
-  
-URL = "http://localhost/certificate/email"
-r = requests.get(URL) 
-
-with open(r'path\certificate.pdf','wb') as f:
-    f.write(r.content)
